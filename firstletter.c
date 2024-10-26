e9f6f4caf0d65b89f1991880169b3dbaea6b571c
Rohit Yadav <rohit.yadav18j@gmail.com>
2024-10-26 12:50:56 +0530

    firstletter.c

--- firstletter.c
+++ firstletter.c
@@ -3,10 +3,16 @@
 int main() {
     char str[100];
     printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin); 
+    fgets(str, sizeof(str), stdin);
+    
+    if (str[0]!='\0')
+    {
+        printf("%c",str[0]);
+    }
+    
     for (int i = 0; str[i]!='\0'; i++)
     {
-        if ( (str[i]) && (i == 0 || (str[i - 1])))
+        if (str[i-1]==' ' || (str[i - 1]=='\n')
         {
             printf("%c",str[i]);
         }
